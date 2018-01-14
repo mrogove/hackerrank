94274515475f915af3a9eeebb6fe824412fa4373
mrogove <mrogove@gmail.com>
2018-01-13 22:47:41 -0700

    added sample input/output for documentation

--- generalPython/lists.py
+++ generalPython/lists.py
@@ -2,6 +2,29 @@
 https://www.hackerrank.com/challenges/python-lists/problem
 
 read in commands; perform those commands to a list.
+
+Sample Input
+
+12
+insert 0 5
+insert 1 10
+insert 0 6
+print
+remove 6
+append 9
+append 1
+sort
+print
+pop
+reverse
+print
+
+Sample Output
+
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+
 """
 if __name__ == '__main__':
 
@@ -23,18 +46,3 @@ if __name__ == '__main__':
 
         else:
             print(l)
-    #
-    # if command=="insert":
-    #     l.insert(i,e)
-    # elif command=="remove":
-    #     l.remove(e)
-    # elif command=="append":
-    #     l.append(e)
-    # elif command=="sort":
-    #     l.sort()
-    # elif command=="pop":
-    #     l.pop()
-    # elif command=="reverse":
-    #     l.reverse()
-    # elif command=="print":
-    #     print(l)
