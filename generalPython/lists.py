46b5bb2dd616fa9154f87731cee58635294d8d47
mrogove <mrogove@gmail.com>
2018-01-14 21:23:45 -0700

    Fixing lists manipulation program. trying to split input by (' ') left me needing to use eval. Default behavior removed excess whitespace.

--- generalPython/lists.py
+++ generalPython/lists.py
@@ -31,18 +31,17 @@ if __name__ == '__main__':
     n = int(input())
     l = [] #initialize
 
-    arg_coms = ['insert', 'append', 'remove']
+    arg_coms = ['insert', 'append', 'remove'] #make more dynamic?
 
     for i in range(n): #for each element entered...
-        row = input().split(' ')
+        row = input().split() #trying to split by (' ') left me needing to use eval. Default behavior removed excess whitespace.
         com = row[0]
 
-        if com in arg_coms:
+        if com != 'print': #try: if len(com) > 1
             args = map(int, row[1:]) #make all args ints via map
             getattr(l, com)(*args) #borrowed this from Dive Into Python
 
-        elif com != 'print':
-            eval("l."+com+"()") #banged head against "()" for too long.
-
         else:
             print(l)
+
+#note: can use getattr() INSTEAD of eval - fix this.
