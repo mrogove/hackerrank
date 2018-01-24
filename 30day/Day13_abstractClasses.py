15d3a1eace7125ef34ffc3f84c725e0acd9e4555
mrogove <mrogove@gmail.com>
2018-01-24 10:13:59 -0500

    HR's 30 day - abstract classes.

---
+++ 30day/Day13_abstractClasses.py
@@ -0,0 +1,44 @@
+"""
+https://www.hackerrank.com/challenges/30-abstract-classes/problem
+Sample Input
+
+The following input from stdin is handled by the locked stub code in your editor:
+
+The Alchemist
+Paulo Coelho
+248
+
+Sample Output
+
+The following output is printed by your display() method:
+
+Title: The Alchemist
+Author: Paulo Coelho
+Price: 248
+
+"""
+from abc import ABCMeta, abstractmethod
+
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self,t,a,p):
+        Book.__init__(self,t,a)
+        self.price = p
+
+    def display(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Price:", self.price)
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
