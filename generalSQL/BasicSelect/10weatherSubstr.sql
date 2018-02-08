cff73c8fcfc5d97410eee2a99dcedb812a9dc771
mrogove <mrogove@gmail.com>
2018-02-08 18:52:27 -0500

    HR's general sql track. Using substr and IN()

---
+++ generalSQL/BasicSelect/10weatherSubstr.sql
@@ -0,0 +1,8 @@
+/*
+	Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+	Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE SUBSTRING(CITY,1,1) in ('a','e','i','o','u');
